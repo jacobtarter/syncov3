3e965c713826ab14c09d04bf4819e87b9de1a1f9
skippy12345 <jaredmurray012@gmail.com>
2016-04-27 12:35:31 -0500

    CSS EXTERNAL TEST

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,28 +5,25 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
 	<script>var baseUrl = "{{ url('/') }}/";</script>
 	<script>var api = "api/v1/"; </script>
-	<link href="{{ asset('css/app.css') }}" media="all" rel="stylesheet" type="text/css" />
+	<link href="{{ asset('http://jmurray.altervista.org/Synco/SyncoCss2.2.css') }}" media="all" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 
-	<h1> TEST </h1>
-	
+	<div class = "img"> TEST </div>
 
 
 
-		<div class="wrapper">
-			<div class="container">
-				<div ng-view></div>
+	<div class="wrapper">
+		<div class="container">
+			<div ng-view></div>
 
 	</div>
 	<script type="text/javascript" src="{{asset('bower_components/angular/angular.min.js')}}"></script>
 	<script type="text/javascript" src="{{asset('bower_components/angular-route/angular-route.min.js')}}"></script>
 	<script type="text/javascript" src="{{asset('bower_components/angular-cookies/angular-cookies.min.js')}}"></script>
 	<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/models.js')}}"></script>
 	<script type="text/javascript" src="{{asset('js/controllers.js')}}"></script>
-
 
 </body>
 </div>
