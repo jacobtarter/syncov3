3c7846d2f1d0071a4b7f4766fc7d6bd0d36aef08
jacobtarter <jtarter01@aurora.edu>
2016-04-27 00:00:42 -0500

    comment form on show page

--- public/build/js/controllers-ca56649044.js
+++ public/build/js/controllers-ca56649044.js
@@ -232,6 +232,28 @@ synco.controller('showController', ['$scope', '$http', '$location', '$routeParam
 			$location.path('/comment/' + id );
 			
 		}
+
+		angular.extend($scope, {
+		makeComment: function(commentForm) {
+		$http({	
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			url: baseUrl + api + "comments",
+			method: "POST",
+			data: {
+				ctext: $scope.post.ctext,
+				c_pid: $scope.id
+			}
+		}).success(function(response) {
+			console.log("comment created, redirecting to home");
+			$location.path('/view/' + $scope.id);
+		}).error(function(data,status,headers) {
+			console.log(data);
+			alert("Error Making Post - Make sure form is filled.");
+		});
+	}
+	});
 	
 
 }]);
