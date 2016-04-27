b2bdc948fc5bf4cafa1fe88abf4f2bd6e2ca8288
jacobtarter <jtarter01@aurora.edu>
2016-04-27 18:14:42 -0500

    limit vote

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -17,19 +17,25 @@ class VoteController extends Controller
 
 	public function store(Request $request)
     {
-        $voteCount = $request->input('uid');
+        $checkVotes = $request->input('uid');
        	$DATA = (array)DB::select( "SELECT * FROM votes WHERE uid = '$voteCount'");
-       	return $DATA;
-        $this->validate($request, array(
-            'v_pid' => 'required',
-            'votescore' => 'required'
-        ));
+       	$voteCount=0;
+        foreach ($DATA as $row){
+            voteCount++;
+        }
+        if(voteCount<1)
+        {
+            $this->validate($request, array(
+                'v_pid' => 'required',
+                'votescore' => 'required'
+            ));
 
-        $vote = new Vote;
-        
-        $vote->v_pid = $request->input('v_pid');
-        $vote->votescore = $request->input('votescore');
-        $vote->uid = $request->input('uid');
-        $vote->save();
+            $vote = new Vote;
+            
+            $vote->v_pid = $request->input('v_pid');
+            $vote->votescore = $request->input('votescore');
+            $vote->uid = $request->input('uid');
+            $vote->save();
+        }
     }
 }
