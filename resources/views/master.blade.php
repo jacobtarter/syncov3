9a41c4ad6ea40639642aba51de4597cdd849d137
jacobtarter <jtarter01@aurora.edu>
2016-04-27 01:11:48 -0500

    user model error

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -23,8 +23,9 @@
 	<script type="text/javascript" src="{{asset('bower_components/angular-route/angular-route.min.js')}}"></script>
 	<script type="text/javascript" src="{{asset('bower_components/angular-cookies/angular-cookies.min.js')}}"></script>
 	<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/controllers.js')}}"></script>
 	<script type="text/javascript" src="{{asset('js/models.js')}}"></script>
+	<script type="text/javascript" src="{{asset('js/controllers.js')}}"></script>
+
 
 </body>
 </div>
