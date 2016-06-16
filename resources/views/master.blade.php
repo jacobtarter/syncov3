b1d4241debaa82595df0b815081940cd543bfa82
jacobtarter <jtarter01@aurora.edu>
2016-06-15 20:16:22 -0500

    css

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,9 +10,13 @@
 
 <body>
 
+	<div id="container">
+
 	<div ng-view></div>
 
 	</div>
+
+
 	<script type="text/javascript" src="{{asset('bower_components/angular/angular.min.js')}}"></script>
 	<script type="text/javascript" src="{{asset('bower_components/angular-route/angular-route.min.js')}}"></script>
 	<script type="text/javascript" src="{{asset('bower_components/angular-cookies/angular-cookies.min.js')}}"></script>
@@ -21,6 +25,5 @@
 	<script type="text/javascript" src="{{asset('js/controllers.js')}}"></script>
 
 </body>
-</div>
-</div>
+
 </html>
