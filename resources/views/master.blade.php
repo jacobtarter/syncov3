198a5c29601a5c51ec3ec78fb1bdff55546bc73d
jacobtarter <jtarter01@aurora.edu>
2016-06-15 20:08:30 -0500

    testing new css

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
 	<script>var baseUrl = "{{ url('/') }}/";</script>
 	<script>var api = "api/v1/"; </script>
-	<link href="{{ asset('css/app.css') }}" media="all" rel="stylesheet" type="text/css" />
+	{{ HTML::style('css/app.css'); }}
 </head>
 
 <body>
