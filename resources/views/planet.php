c72b88d460cdd2b649134e297a0d049692cbff13
jacobtarter <jtarter01@aurora.edu>
2016-09-27 14:44:52 -0500

    scitech

--- resources/views/planet.php
+++ resources/views/planet.php
@@ -5,12 +5,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
 <body>
-  <h1>What is a planet?</h1>
+  <h1 style="color:white" "border-style 1px solid black">What is a planet?</h1>
   <div class="scitechcontainer">
-  <p class="description">All planets have 4 things that describe them - they orbit the sun, they don’t orbit another planet,
+  <div class="description">All planets have 4 things that describe them - they orbit the sun, they don’t orbit another planet,
     they are round (or mostly round), and they can push other objects with their gravity.  There are also
     dwarf planets - these are almost the same, except they are not large enough to push other objects like a planet can.
-  </p>
+  </div>
 </div>
 </body>
 </html>
