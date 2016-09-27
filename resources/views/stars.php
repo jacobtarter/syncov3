fba7cdceecb9b7d42ca6e105e9cb2898e22257ce
jacobtarter <jtarter01@aurora.edu>
2016-09-27 18:39:50 -0500

    css

--- resources/views/stars.php
+++ resources/views/stars.php
@@ -9,15 +9,18 @@
 
     <h1 class="scitechtitle"> What Are Constellations?</h1>
 
-    <div class="description">Constellations are groups of stars that are visible from Earth.  The Northern
+    <div class="description">
+      <ul class="plainlist">
+        <li>Constellations are groups of stars that are visible from Earth.  The Northern
       and Southern Hemispheres have their own sets of constellations.  Since the same stars are visible
       from so many parts of the world, many different cultures have identified and named constellations
-      throughout history - sometimes the same stars can have more than one name.  Constellations have
+      throughout history - sometimes the same stars can have more than one name.</li>
+      </li>Constellations have
       been useful to us for determining seasons, creating calendars, and for navigation.  One example
       is the Big Dipper, which is often used for locating Polaris - the “North Star”.  This star appears
       to stay almost motionless by our north pole, which  has helped people know which way was North
-      before they had the compass.
-
+      before they had the compass.</li>
+    </ul>
   </div>
 </body>
 </html>
