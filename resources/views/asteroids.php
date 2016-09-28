7cc71b1bbb0ba4eb40db84271b441650d9d91238
jacobtarter <jtarter01@aurora.edu>
2016-09-28 12:08:23 -0500

    format

--- resources/views/asteroids.php
+++ resources/views/asteroids.php
@@ -12,13 +12,17 @@
     <div class="description">
       <div class="textsection">Asteroids are made up of bits and pieces that were left over from when the Solar System was formed,
       which means that scientists can use them like a time capsule.</div>
+      <hr>
       <div class="textsection">Comets are made of frozen gas, dirt, and rock.  They can be the size of a small town, with tails
           that stretch millions of miles!  Some comets take hundreds of years just to orbit the Sun once.</div>
+      <hr>
       <div class="textsection">Meteoroids are objects made of rocky or metal material, and they travel through the solar system.
           They can be as small as a grain of rice, or as large as a car!</div>
+      <hr>
       <div class="textsection">If a meteoroid enters the Earth’s atmosphere, but gets burned up before it can reach
         the surface, it is called a meteor.  Meteor showers are the flashes of light created by the meteors getting
         vaporized.</div>
+      <hr>
       <div class="textsection">If a meteoroid makes it all the way through our atmosphere and hits the surface of
         Earth, it is called a meteorite.</div>
     </div>
