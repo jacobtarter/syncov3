aed3b32804ba4519fa9567ac63a7491b3c207533
jacobtarter <jtarter01@aurora.edu>
2016-09-27 21:14:13 -0500

    links

--- resources/views/scitechmain.php
+++ resources/views/scitechmain.php
@@ -12,15 +12,17 @@
   <div class="description">
       <div class="textsection"></b>Current Pages for QR Scanning</b></div>
       <div class="textsection">
-          <a href="/scitech/planet">Planets</a>
-          <a href="/scitech/stars">Constellations</a>
-          <a href="/scitech/asteroids">Asteroids, Comets & Meteoroids</a>
-          <a href="/scitech/solarsys">Our Solar System</a>
-          <a href="/scitech/planetsci">Planetary Science</a>
-          <a href="/scitech/planetsize">Planet Sizes</a>
-          <a href="/scitech/moons">Moons & Satellites</a>
-          <a href="/scitech/galaxy">Stars & Galaxies</a>
-          <a href="/scitech/darkmatter">Dark Matter</a>
+        <ul>
+          <li><a href="/scitech/planet">Planets</a></li>
+          <li><a href="/scitech/stars">Constellations</a></li>
+          <li><a href="/scitech/asteroids">Asteroids, Comets & Meteoroids</a></li>
+          <li><a href="/scitech/solarsys">Our Solar System</a></li>
+          <li><a href="/scitech/planetsci">Planetary Science</a></li>
+          <li><a href="/scitech/planetsize">Planet Sizes</a></li>
+          <li><a href="/scitech/moons">Moons & Satellites</a></li>
+          <li><a href="/scitech/galaxy">Stars & Galaxies</a></li>
+          <li><a href="/scitech/darkmatter">Dark Matter</a></li>
+        </ul>
       </div>
   </div>
 </body>
