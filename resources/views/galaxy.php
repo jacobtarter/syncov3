9c05e6b5e532b9d944ed80694b9b8689ee3434e3
jacobtarter <jtarter01@aurora.edu>
2016-09-27 20:39:48 -0500

    format

--- resources/views/galaxy.php
+++ resources/views/galaxy.php
@@ -12,9 +12,9 @@
     <div class="description">
       <div class="textsection">You learned how a solar system is a group of planets orbiting a sun.  A galaxy is
           similar but much bigger - it is made up of a group of stars and other objects which
-          are held together by the gravitational forces of a dense object in its center, such as a black hole</div>
+          are held together by the gravitational forces of a dense object in its center, such as a black hole.</div>
       <div class="textsection">The pressure inside a star is so great that it forces atoms together into bigger atoms - This process
-          is called fusion, and it creates a massive amount of energy for the star to run on - like fuel.</div>
+          is called fusion, and it creates a massive amount of energy for the star to use as fuel.</div>
       <div class="textsection">Did you know that all matter in the Universe is created by the fusion
           of stars? This means that everything here on Earth is made up of star-stuff - including you! </div>
     </div>
