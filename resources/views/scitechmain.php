5dc6d9cafc650064bd3161a4d4e6e864d6080c1a
jacobtarter <jtarter01@aurora.edu>
2016-09-27 21:12:34 -0500

    css

--- resources/views/scitechmain.php
+++ resources/views/scitechmain.php
@@ -8,7 +8,20 @@
   <div class="scitechcontainer">
   <h1 class="scitechtitle">Temporary SciTech QR Website</h1>
 
-  <div class="description">This website will be the temporary home for SciTech's QR code information
-</div>
+  <div class="scitechtitle">This website will be the temporary home for SciTech's QR code information</div>
+  <div class="description">
+      <div class="textsection"></b>Current Pages for QR Scanning</b></div>
+      <div class="textsection">
+          <a href="/scitech/planet">Planets</a>
+          <a href="/scitech/stars">Constellations</a>
+          <a href="/scitech/asteroids">Asteroids, Comets & Meteoroids</a>
+          <a href="/scitech/solarsys">Our Solar System</a>
+          <a href="/scitech/planetsci">Planetary Science</a>
+          <a href="/scitech/planetsize">Planet Sizes</a>
+          <a href="/scitech/moons">Moons & Satellites</a>
+          <a href="/scitech/galaxy">Stars & Galaxies</a>
+          <a href="/scitech/darkmatter">Dark Matter</a>
+      </div>
+  </div>
 </body>
 </html>
