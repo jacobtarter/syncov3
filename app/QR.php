67768e56fd01c9034cf4ba298b3a40a0ab6ebbb6
jacobtarter <jtarter01@aurora.edu>
2016-09-29 18:21:59 -0500

    test

--- app/QR.php
+++ app/QR.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class QR extends Model
 {
+    public $table = "qr";
     protected $fillable = array('qr_id', 'title', 'description', 'link', 'created_at', 'updated_at');
 }
