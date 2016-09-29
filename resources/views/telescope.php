5033fb46598663fbade0d5a215e8d9caa0474ba3
jacobtarter <jtarter01@aurora.edu>
2016-09-29 14:38:10 -0500

    telescope

--- resources/views/telescope.php
+++ resources/views/telescope.php
@@ -13,11 +13,11 @@
       <div class="textsectionfirst">Our eyes need light in order to see.  Galileo’s telescope uses two different
         types of lenses to capture more light than our eyes can, and then focus that light back into an image
         that looks clear to us.  When our eyes get more light, we can see better!</div>
-  
+
       <div class="textsectionlast">The lens at the end of the telescope is convex.  When light goes through a
-        convex lens, it collects it and focuses it down to a single point.  The lens that your eye looks
-        through is concave.  Concave lenses spread the light back out, and directs it to your eye as a clear
-        picture.</div>
+        convex lens, it collects it and focuses it down to a single point.  The lens that your eye looks through
+        is concave.  Concave lenses spread the light back out.  This lens takes the single point of light and
+        turns it into parallel light rays, which makes it look clear to our eye.</div>
     </div>
 
   </div>
