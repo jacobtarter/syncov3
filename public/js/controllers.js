10cff0d57f497eb7fba5c07c850d3a2979b418fe
jacobtarter <jtarter01@aurora.edu>
2016-09-29 17:06:11 -0500

    test

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -549,4 +549,62 @@ synco.controller('adminController', ['$scope', '$http', '$location', '$routePara
 
 }]);
 
+synco.controller('qrcontroller', ['$scope', '$http', '$location', 'userModel', function($scope, $http, $location, userModel) {
+  $scope.qrData = {};
+
+
+  $scope.loadData = function()
+	{
+		$http.get("http://www.synco.xyz/sciapi/v1/qr")
+		.success(function(response) {
+			$scope.qrData = response;
+		});
+	};
+
+  $scope.loadData();
+
+  $scope.makeQR = function()
+  {
+
+
+    $http({
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: "http://www.synco.xyz/sciapi/v1/qr",
+      method: "POST",
+      data: {
+        title: $scope.qr.title,
+        ptext: $scope.qr.description,
+        name: $scope.qr.link
+      }
+    }).success(function(response) {
+      console.log("post created, redirecting to home");
+      $location.path('/');
+
+    }).error(function(data,status,headers) {
+      console.log(status);
+      console.log(data);
+      console.log(headers);
+
+      if(status==429)
+      {
+        //alert("Too many API hits, throttle limit hit.")
+        console.log(data);
+        var json = 'http://ipv4.myexternalip.com/json';
+        $http.get(json).then(function(result) {
+            console.log(result.data.ip)
+            alert("You have hit the API's post limit. Your ip address has been logged: " + result.data.ip);
+        });
+
+      }
+      else{
+      alert("Error Making Post - Make sure form is filled.");
+      }
+    });
+
+  }
+
+  }]);
+
 //# sourceMappingURL=controllers.js.map
