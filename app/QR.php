4eae4b90e3af25cb5c5fc5bde6a5ee4e2f81f2f1
jacobtarter <jtarter01@aurora.edu>
2016-09-29 18:17:34 -0500

    test

--- app/QR.php
+++ app/QR.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class QR extends Model
 {
     protected $fillable = array('qr_id', 'title', 'description', 'link', 'created_at', 'updated_at');
 }
