79bd960165b5b4d7842c74881ba031ebe77d37fb
jacobtarter <jtarter01@aurora.edu>
2016-09-29 21:36:00 -0500

    test

--- public/build/js/controllers-548bb9e8a8.js
+++ public/build/js/controllers-548bb9e8a8.js
@@ -618,13 +618,15 @@ synco.controller('showqrcontroller', ['$scope', '$http', '$location', '$routePar
   $scope.qr = {};
   $scope.id = $routeParams.id;
 
-  console.log("http://www.synco.xyz/sciapi/v1/qr/" + $scope.id);
+  console.log("get: http://www.synco.xyz/sciapi/v1/qr/" + $scope.id);
   $http.get("http://www.synco.xyz/sciapi/v1/qr/" + $scope.id )
   .success(function(response){
     $scope.qr= response;
+      console.log("response: " + response);
   }).error(function(response) {
     alert("error getting your json");
   });
+
 
 
 
