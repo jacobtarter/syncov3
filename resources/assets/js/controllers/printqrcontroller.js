6762ef2ae839c1aa4ad846e9a3a74e22672ba68f
jacobtarter <jtarter01@aurora.edu>
2016-10-11 13:59:48 -0500

    qr

--- resources/assets/js/controllers/printqrcontroller.js
+++ resources/assets/js/controllers/printqrcontroller.js
@@ -5,7 +5,7 @@ synco.controller('printqrcontroller', ['$scope', '$http', '$location', '$routePa
   $scope.size=1000;
   $scope.linkforqr = "http://www.synco.xyz/sciapi/v1/qr/" + $scope.id;
   $scope.qrapi = "http://api.qrserver.com/v1/create-qr-code/?data=";
-  $scope.qrsize = $scope.size + "x" + $scope.size;
+  $scope.qrsize = "&amp;size=" + $scope.size + "x" + $scope.size;
 
   console.log($scope.qrapi + $scope.linkforqr + $scope.qrsize);
   $scope.qrsource= $scope.qrapi + $scope.linkforqr + $scope.qrsize;
