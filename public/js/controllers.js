b142a0801a0976d718f97c287ec66a61d2cef719
jacobtarter <jtarter01@aurora.edu>
2016-10-11 12:49:48 -0500

    qr

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -649,14 +649,9 @@ synco.controller('printqrcontroller', ['$scope', '$http', '$location', '$routePa
   $scope.id = $routeParams.id;
   $scope.size=500;
 
-  console.log("get: http://www.synco.xyz/sciapi/v1/qr/" + $scope.id);
-  $http.get("http://api.qrserver.com/v1/create-qr-code/?data=" + "http://www.synco.xyz/sciapi/v1/qr/" + $scope.id + "!&size=" + $scope.size + "x" + $scope.size )
-  .success(function(response){
-    $scope.qr= response;
 
-  }).error(function(response) {
-    alert("error getting your json");
-  });
+  $scope.qrsource= "http://api.qrserver.com/v1/create-qr-code/?data=" + "http://www.synco.xyz/sciapi/v1/qr/" + $scope.id + "!&size=" + $scope.size + "x" + $scope.size;
+
 
   $scope.playSound = function(){
     $scope.sound.play();
